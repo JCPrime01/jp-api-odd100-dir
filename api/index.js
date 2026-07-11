9012fc8b09dcfa462cd59f0f07b42193c82808df
JCPrime01 <vanzokiller@gmail.com>
2026-07-11 13:26:53 -0300

    chore: shareCode 6L6VIGQL3WI -> D2X4Q502MGP (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=6L6VIGQL3WI&afp2=tp' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=D2X4Q502MGP&afp2=tp' });
     res.end();
   }
