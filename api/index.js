2068552c326faff40ff567630a95e2162905ce01
JCPrime01 <vanzokiller@gmail.com>
2026-07-22 23:14:52 -0300

    chore: shareCode 6L9RUAEEPJB -> EETW589DLXS (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=6L9RUAEEPJB&afp2=tp' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=EETW589DLXS&afp2=tp' });
     res.end();
   }
