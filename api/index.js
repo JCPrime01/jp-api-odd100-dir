5e62fcb8219f3041824019463643555e413273ac
JCPrime01 <vanzokiller@gmail.com>
2026-08-04 00:01:29 -0300

    chore: shareCode DLDVYDLDETF -> OSH985O4PO3 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=DLDVYDLDETF&afp2=tp' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=OSH985O4PO3&afp2=tp' });
     res.end();
   }
